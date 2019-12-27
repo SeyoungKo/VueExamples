4ec932afcf1aa53b0854856442c64ee6e9089640
seyoung <airseyoung@naver.com>
2019-12-27 17:35:12 +0900

    remove : all file remove

--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-// 컴포넌트에서 state 값을 가져갈 때에 대한 정의
-
-// ex > 배열의 순서 뒤집기 등등 ..
-
-export const messages = state => state.messages.reverse()
-export const channels = state => state.channels
--- src/store/mutation-types.js
+++
@@ -1,3 +0,0 @@
-export const SET_MESSAGE = "SET_MESSAGE"
-export const GET_CHANNELS = "GET_CHANNELS"
-

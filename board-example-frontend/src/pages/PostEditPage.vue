d0f3f819bcb77c6b4d8ccfb8c41807cbf3d5817f
seyoung <airseyoung@naver.com>
2020-01-12 01:32:55 +0900

    Revert "Add : PostEditPage PUT method"
    This reverts commit fadc8e17d9f25af16a01b1422867cb9a156448a3.

--- board-example-frontend/src/pages/PostEditPage.vue
+++ board-example-frontend/src/pages/PostEditPage.vue
@@ -35,31 +35,7 @@ export default {
     methods:{
         onSubmit(payload){
             // PostEditForm의 submit 이벤트 헨들러인 onSubmit()
-            const {title, contents} = payload
-            //PUT 메소드를 사용해 서버로 게시물 데이터를 전송한다.
-            api.put(`/posts/${this.postId}`,{title, contents}).then(res=>{
-              // 게시물 수정이 완료되면 게시물 페이지로 이동시킨다.
-               alert('게시물이 성공적으로 수정되었습니다.')
-               this.$router.push({
-                   name:'PostViewPage',
-                   params: {PostId:res.data.id.toString()}
-               })
-            }).catch(err=>{
-                if(err.response.status ===401){
-                    // 비로그인 접근 방지
-                alert('로그인이 필요합니다.')
-                this.$router.push({
-                    name:'Signin'
-                 })
-             }else if(err.response.status ===403){
-                 // 사용자가 이 게시물에 대해 수정권한이 없다면 이전페이지로 이동시킨다.
-                 alert(err.response.ddata.msg)
-                 this.$router.back() // 이전 페이지
-             }else{
-                 // 그 외 경우 서버에서 보내준 에러 메세지를 노출시킨다.
-                 alert(err.response.data.msg)
-             }
-          })
+            console.log(payload)
         }
     }
 }
